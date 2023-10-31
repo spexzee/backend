eb494aa3117e654d0e2f336df14f6a251e3c64ea
spexzee <abub5979@gmail.com>
2023-10-31 14:51:40 +0530

    update notes

--- routes/notes.js
+++ routes/notes.js
@@ -13,8 +13,8 @@ router.get('/fetchallnotes', fetchuser, async (req, res) => {
     res.json(notes)
 })
 
-//Route1
-//add notes : POST: '/api/notes/addnote' login required
+//Route2
+//add note : POST: '/api/notes/addnote' login required
 router.post('/addnote', fetchuser, [
     body('title', 'Enter valid title').isLength({ min: 3 }),
     body('description', 'description must be atleast 5 characters').isLength({ min: 5 }),
@@ -28,11 +28,49 @@ router.post('/addnote', fetchuser, [
     const { title, description, tag } = req.body
     try {
 
-        const notes = await Notes.create({
+        const note = new Notes({
             user: req.user.id, title, description, tag
         })
-        const saveNote = await notes.save(notes)
+        const saveNote = await note.save(note)
         res.send(saveNote)
+    }
+    catch (error) {
+        console.error(error.message);
+        res.status(500).send('internal server error')
+    }
+
+})
+
+
+//Route3
+//update note : PUT: '/api/notes/updatenote' login required
+router.put('/updatenotes/:id', fetchuser, async (req, res) => {
+    //if error occures give bad request and error message
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+
+    const { title, description, tag } = req.body
+
+    try {
+        //create new note object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        //find the note and uodate
+        let note = await Notes.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") };
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed")
+        }
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note })
+
     }
     catch (error) {
         console.error(error.message);
