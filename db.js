37af77b5a5cb0a7da7a5429858dd7ea43a6e8bfc
Spexzee_Abu <104344646+spexzee@users.noreply.github.com>
2024-04-04 13:28:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 
 async function connectToMongo() {
     try {
-        const Mongo_url = process.env.MONGODB_URL
-
+        const Mongo_url = "mongodb+srv://spexzee:Spexzee786@clusternote.jjk6c0u.mongodb.net/?retryWrites=true&w=majority"
         await mongoose.connect(`${Mongo_url}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
