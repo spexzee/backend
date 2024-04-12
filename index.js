9e9a8411c26800e2a66bd1f92666a7f722a40848
Spexzee_Abu <104344646+spexzee@users.noreply.github.com>
2024-04-12 16:17:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors(
 {
-    origin: 'https://spexzee-notebook.netlify.app',
+    origin: '*',
     methods: ["POST", "GET" , "PUT" , "DELETE"],
     credentials: true
 }));
